481f095321c9905730750e4130f1fbd13e29368d
Pavithra820 <56164471+Pavithra820@users.noreply.github.com>
2024-01-09 16:17:01 +0530

    Add files via upload

--- javascript/cart.js
+++ javascript/cart.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, collection, getDocs, deleteDoc,doc,addDoc,getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getFirestore, collection, getDocs, updateDoc,doc,addDoc,getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
  
 const firebaseConfig = {
   apiKey: "AIzaSyAru6JgHWgmu9eMdCi2b9eP7R8xLOxteqA",
@@ -10,7 +10,7 @@ const firebaseConfig = {
   appId: "1:195944459124:web:ee7f54a1a87ef193119a21",
   measurementId: "G-SYHPGRBD62",
 };
-
+ 
 //const user=localStorage.getItem("user");
 const user=localStorage.getItem("user");
 const app = initializeApp(firebaseConfig);
@@ -25,25 +25,109 @@ const docSnapshot = await getDoc(docRef);
 let totalPrice = 0;
 const shippingCharge = 1000;
 let orderTotal = 0;
+
+let check = parseInt(localStorage.getItem("check"));
 let cartId=[];
+if (check === 1) {
+  const user = localStorage.getItem("user");
+  const docRef = doc(db, 'User', user);
+  
+  // Use async function to handle promises
+  (async () => {
+    try {
+      const docSnapshot = await getDoc(docRef);
 
-// Function to delete a document from the "Cart" collection
-function deleteItemFromCart(itemId) {
-    console.log(itemId);
-    const collectionRef = collection(db, "User");
-    const itemRef = doc(collectionRef, itemId);
-
-    deleteDoc(itemRef);
+      if (docSnapshot.exists()) {
+        cartId=docSnapshot.data().cart;
+        console.log(cartId.length);
+        if(cartId.length==0){
+          document.getElementById("opencart").style.display="";
+          document.getElementById("closecart").style.display="none";
+          document.getElementById("number").style.display="none";
+        }
+        else{
+          document.getElementById("opencart").style.display="none";
+          document.getElementById("closecart").style.display="";
+          document.getElementById("number").style.display="";
+          document.getElementById("number").style.marginLeft="10px";
+          document.getElementById("number").innerHTML=cartId.length;
+        }
+        var newname = docSnapshot.data().name;
+        newname = newname.charAt(0).toUpperCase() + newname.slice(1);
+        document.getElementById("login").innerHTML = `<i class="fa fa-user" style="font-size:28px;"><p style="font-family: sans-serif;"><b>${newname}</b></p></i>`;
+        document.getElementById("firstoption").innerHTML = `<i class="fa fa-user" style="margin-left: -50px;font-size:18px;" aria-hidden="true"><span style="font-family: sans-serif;margin-left: 10px;"><b>${newname}</b></span></i>`;
+        let isDropdownOpen = false;
+        document.getElementById("login").addEventListener("click", function () {
+          const dropdown = document.getElementById("dropdown");
+          if (isDropdownOpen) {
+            dropdown.style.display = "none";
+          } else {
+            dropdown.style.display = "block";
+          }
+          // Toggle the dropdown state
+          isDropdownOpen = !isDropdownOpen;
+        });
+        // Close the dropdown when clicking outside of it
+        document.addEventListener("click", function (event) {
+          const dropdown = document.getElementById("dropdown");
+          if (event.target.closest("#login") || event.target.closest("#dropdown")) {
+            return;
+          }
+          // Clicked outside the login button and dropdown, close the dropdown
+          dropdown.style.display = "none";
+          isDropdownOpen = false;
+        });
+      } else {
+        console.error("Document does not exist.");
+      }
+    } catch (error) {
+      console.error("Error getting document:", error);
+    }
+  })();
 }
-
+else {
+  document.getElementById("opencart").style.display="none";
+  document.getElementById("closecart").style.display="none";
+  document.getElementById("number").style.display="none";
+  document.getElementById("login").addEventListener("click", function () {
+    window.location.href = "../html/login.html";
+  });
+}
+ 
+// Function to delete a document from the "Cart" collection
+async function deleteItemFromCart(itemId) {
+  console.log(itemId);
+  const userId=localStorage.getItem("user");
+  const userRef = doc(collection(db, "User"), userId);
+  const userDoc = await getDoc(userRef);
+  if (userDoc.exists()) {
+      // Get the user's cart array
+      const cartArray = userDoc.data().cart || [];
+      // Log the cart items
+      console.log("Current cart:", cartArray);
+      // Find the index of the item to be removed
+      const indexToRemove = cartArray.indexOf(itemId);
+      if (indexToRemove !== -1) {
+          cartArray.splice(indexToRemove, 1);
+          console.log("Item removed from cart");  
+          await updateDoc(userRef, { cart: cartArray });
+          document.getElementById("number").innerHTML=cartArray.length;
+      } else {
+          console.log("Item not found in the cart");
+      }
+  } else {
+      console.log("User not found");
+  }
+}
+ 
 // Function to add a document to the "SaveForLater" collection
 async function saveItemForLater(itemData) {
     await addDoc(saveForLaterCollection, itemData);
 }
-
+ 
 async function getId(user) {
   try {
-
+ 
     if (docSnapshot.exists()) {
       return docSnapshot.data().cart;
     } else {
@@ -55,12 +139,12 @@ async function getId(user) {
     return null;
   }
 }
-
+ 
 getDocs(storeWatchesRef)
   .then(async (querySnapshot) => {
     const user = localStorage.getItem("user");
-    const cartId = await getId("pavi@gmail.com"); // Get the cartId
-
+    const cartId = await getId(user); // Get the cartId
+ 
     querySnapshot.forEach((doc) => {
       // Check if the item is in the user's cart
       if (cartId.includes(doc.id)){
@@ -71,10 +155,10 @@ getDocs(storeWatchesRef)
         const formatPrice = priceData.toLocaleString("en-IN");
         const price = "Rs."+formatPrice;
         const itemId = doc.id; // Save the document ID for later reference
-
-        
+ 
+       
         const itemDataDiv = document.createElement("div");
-      
+     
         itemDataDiv.className = "item-data";
         itemDataDiv.innerHTML = `
          <div class=maindiv>
@@ -105,46 +189,41 @@ getDocs(storeWatchesRef)
         totalPrice += itemPrice;
         orderTotal = totalPrice + shippingCharge;
         itemsListContainer.appendChild(itemDataDiv);
-
+ 
         // Add a click event listener to the trash icon
         const trashIcon = itemDataDiv.querySelector(".icon-trash");
-        trashIcon.addEventListener("click", () => {
-          // Delete the corresponding document from the "Cart" collection
+        trashIcon.addEventListener("click", async () => {
           deleteItemFromCart(itemId);
-
-          // Delete the item from the page
           itemDataDiv.remove();
-
-          // Update the total prices
           totalPrice -= itemPrice;
           orderTotal = totalPrice + shippingCharge;
-
+ 
           // Update the displayed totals
           displaySubtotal(totalPrice);
           displayTotalPrice(orderTotal);
         });
-
+ 
         // Add a click event listener to the save icon
         const saveIcon = itemDataDiv.querySelector(".icon-save");
         saveIcon.addEventListener("click", () => {
           // Save the corresponding document to the "SaveForLater" collection
           saveItemForLater(doc.data());
-
+ 
           // Optionally, you can remove the item from the cart after saving for later
           deleteItemFromCart(itemId);
           itemDataDiv.remove();
-
+ 
           // Update the total prices
           totalPrice -= itemPrice;
           orderTotal = totalPrice + shippingCharge;
-
+ 
           // Update the displayed totals
           displaySubtotal(totalPrice);
           displayTotalPrice(orderTotal);
         });
       }}
     );
-
+ 
     // Display the initial subtotal and total
     displaySubtotal(totalPrice);
     displayTotalPrice(orderTotal);
@@ -152,9 +231,9 @@ getDocs(storeWatchesRef)
   .catch((error) => {
     console.error("Error getting documents: ", error);
   });
-
+ 
 function displaySubtotal(subtotal) {
-
+ 
   const formatSubtotal=subtotal.toLocaleString("en-IN");
   const subtotall="Rs."+formatSubtotal;
   const subTotalDiv = document.getElementById('amount');
@@ -166,29 +245,29 @@ function displayTotalPrice(orderTotal) {
   const ordertotall="Rs."+formatOrdertotal;
   const orderDiv = document.getElementById('total');
   orderDiv.textContent =  ordertotall;
-
+ 
 }
  
-
+ 
 // Use async function to handle promises
 (async () => {
     try {
         const docSnapshot = await getDoc(docRef);
-
+ 
         if (docSnapshot.exists()) {
             var newname = docSnapshot.data().name;
             newname = newname.charAt(0).toUpperCase() + newname.slice(1);
             console.log(newname);
             document.getElementById("login").innerHTML = `<i class="fa fa-user"  aria-hidden="true"><span style="margin-left:9px" ">${newname}</span></i>`;
-            
+           
         }
     } catch (error) {
         console.error("Error fetching user document:", error);
     }
 })();
-
+ 
 localStorage.setItem("userName", user);
-
+ 
     $(document).ready(function(){
             $('.carousel').slick({
                 autoplay: true, // Set to true for automatic slideshow
@@ -200,7 +279,3 @@ localStorage.setItem("userName", user);
                 slidesToScroll: 1 // Number of slides to scroll at a time
             });
         });
-
-
-
-
